5d1e289468bafe3c35a43f3302f0461447c9a1b5
Yong Sheng Tan <yong.tan@symphony.com>
2020-03-05 13:27:39 +0800

    Vinay's Code

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,8 +1,11 @@
 const clickToCallName = 'clickToCall:controller';
 const clickToCallService = SYMPHONY.services.register(clickToCallName);
+// const baseUrl = 'https://localhost:4000';
+// ./ngrok https 4000
+const baseUrl = 'https://cac073b8.ngrok.io';
 
 let appToken = undefined;
-let appTokenPromise = fetch('https://localhost:4000/appToken')
+let appTokenPromise = fetch(`${baseUrl}/appToken`)
   .then(res => res.json())
   .then(res => {
     appToken = res['token'];
@@ -10,31 +13,62 @@ let appTokenPromise = fetch('https://localhost:4000/appToken')
 
 Promise.all([ appTokenPromise, SYMPHONY.remote.hello()]).then(data => {
   SYMPHONY.application.register(
-      { appId: 'click-to-call', tokenA: appToken },
+      { appId: 'sfdc-ui-app', tokenA: appToken },
       [ 'ui', 'dialogs' ],
       [ clickToCallName ]
   )
   .then(function (response) {
     const uiService = SYMPHONY.services.subscribe('ui');
     const dialogsService = SYMPHONY.services.subscribe('dialogs');
-    const noPhoneMessage = `<dialog>No phone information for this user<br /><br /></dialog>`;
+    const notConfiguredMessage = `<dialog><h1>Configuration Error</h1><br/>SalesForce has not been configured for this chat<br /><br /></dialog>`;
     const callButton = {
-      icon: 'https://localhost:4000/img/icon_small.png',
-      label: 'Click to Call',
+      icon: `${baseUrl}/img/salesforce-icon.jpg`,
+      label: 'SalesForce',
       data: {}
     };
-    uiService.registerExtension('profile', 'clickToCall-profile', clickToCallName, callButton);
+    // uiService.registerExtension('profile', 'clickToCall-profile', clickToCallName, callButton);
     uiService.registerExtension('single-user-im', 'clickToCall-im', clickToCallName, callButton);
+    uiService.registerExtension('room', 'clickToCall-room', clickToCallName, callButton);
 
     clickToCallService.implement({
       trigger: function(uiClass, id, payload, data) {
-        if (!payload.user.phone) {
-          dialogsService.show('noPhoneDialog', 'noPhoneService', noPhoneMessage, {}, {});
-          return;
-        }
+          // Sapphire Investments
+          if (payload.threadId == '9xRmFY+MxFx1Hmg8aFDq5n///pHJjydKdA==') {
+          console.log('Button was clicked on ', JSON.stringify(payload));
+          window.open('https://symdev5-dev-ed.lightning.force.com/00T/e?sObjectName=Task&save_new_url=%2F00T%2Fe&navigationLocation=LIST_VIEW');
+          return
+          }
+          // Case Management use case
+          if (payload.threadId == 'OvZX5/C8qFgh5yFCk/IF8H///pHFIPSTdA==') {
+            console.log('Button was clicked on ', JSON.stringify(payload));
+            window.open('https://symdev5-dev-ed.lightning.force.com/lightning/o/Case/new?nooverride=1&useRecordTypeCheck=1&navigationLocation=LIST_VIEW&backgroundContext=%2Flightning%2Fr%2FCase%2F5001I00000PjkqDQAR%2Fview');
+            return
+          }
+          // INT Montchard
+          if (payload.threadId == 'ddztFvb3CEwXgwV+E0qQpX///pP6gBERdA==') {
+          console.log('Button was clicked on ', JSON.stringify(payload));
+          window.open('https://symdev5-dev-ed.lightning.force.com/lightning/o/Task/new?nooverride=1&useRecordTypeCheck=1&navigationLocation=LIST_VIEW&backgroundContext=%2Flightning%2Fr%2FCase%2F5001I00000PjkqDQAR%2Fview');
+          return
+          }
+          // NCG Capital
+          if (payload.threadId == 'fVjyngldp6+7N4ak1J2wHn///pQagLS1dA==') {
+          console.log('Button was clicked on ', JSON.stringify(payload));
+          window.open('https://symdev5-dev-ed.lightning.force.com/lightning/o/Task/new?nooverride=1&useRecordTypeCheck=1&navigationLocation=LIST_VIEW&backgroundContext=%2Flightning%2Fr%2FCase%2F5001I00000PjkqDQAR%2Fview');
+          return
+          }
+
+          // 1-1 SFDC Bot
+          if (payload.threadId == 'wobY4upNxIF9C8JU7R4hOX///pVFSinKdA==') {
+          console.log('Button was clicked on ', JSON.stringify(payload));
+          window.open('https://symdev5-dev-ed.lightning.force.com/lightning/o/Task/new?nooverride=1&useRecordTypeCheck=1&navigationLocation=LIST_VIEW&backgroundContext=%2Flightning%2Fr%2FCase%2F5001I00000PjkqDQAR%2Fview');
+          return
+          }
 
         // TODO: Implement integration with third-party telephony API here
-        window.location.assign(`dialpad:${payload.user.phone}`);
+        // window.open('https://www.bbc.co.uk/news');
+          console.log('Last Button was clicked on ', JSON.stringify(payload));
+          dialogsService.show('notConfigured', 'notConfigured', notConfiguredMessage, {}, {});
+          return;
       }
     })
   })
